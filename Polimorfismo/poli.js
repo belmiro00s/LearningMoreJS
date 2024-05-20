2b20d1fdda15f22321e6e3fa166500b0b88acccb
belmiro00s <guilhermebelmiro7@gmail.com>
2024-05-20 08:13:15 -0300

    Polymorphism

---
+++ Polimorfismo/poli.js
@@ -0,0 +1,35 @@
+//Polymorphism in this case, is methods behaving in different ways
+
+
+class Vehicle {
+     move(){
+        console.log('The Vehicle is moving')
+     }
+}  
+
+
+class car extends Vehicle {  // extends Vehicle class to use and change behavior of move method. 
+    move(){
+        console.log('The car is moving')
+    }   
+}
+
+
+class ship extends Vehicle {
+    move(speed){ // we can pass parameters as well
+        console.log(`The ship is moving at ${speed}KM per hour`)
+    }   
+}
+
+
+class bike extends Vehicle {
+    move(){
+        console.log('The bike is moving')
+    }   
+}
+
+
+
+
+
+
