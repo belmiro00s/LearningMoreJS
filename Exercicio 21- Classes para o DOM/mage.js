9f70f2de6801833664db82b97bd9f52194def5e4
belmiro00s <guilhermebelmiro7@gmail.com>
2024-06-01 10:20:26 -0300

    mage and thief

--- Exercicio 21- Classes para o DOM/mage.js
+++ Exercicio 21- Classes para o DOM/mage.js
@@ -2,7 +2,20 @@ import { Character } from "./game";
 
 class Mage extends Character{
     constructor(health, attackPoints, defensePoints, magicPoints){
-        super('Mage', health, attackPoints, defensePoints, magicPoints)
+        this.magicPoints = magicPoints
+        super('Mage', health, attackPoints, defensePoints, magicPoints)    
+    }
+
+
+    attack(targetCharacter) {
+        const damage = targetCharacter.defensePoints - (this.attackPoints + this.magicPoints)
+        targetCharacter.health -= damage
         
+        console.log(`${targetCharacter.name} have ${targetCharacter.health} points of life left.`)
+        
+    }
+
+    heal(targetCharacter) {
+        targetCharacter.health = this.magicPoints * 2
     }
 }
--- Exercicio 21- Classes para o DOM/thief.js
+++ Exercicio 21- Classes para o DOM/thief.js
@@ -7,23 +7,18 @@ class Thief extends Character{
     }
 
     attack(target){
-        const damage = 2 * (this.attackPoints - target.defensePoints) // subtraindo o atack (2x maior) pela defesa do alvo
+        const damage =  (this.attackPoints - target.defensePoints) * 2 // subtraindo o atack (2x maior) pela defesa do alvo
 
-        const actualDamage = Math.max(damage, 0); // Garante que o dano não seja negativo
 
-        target.health -= actualDamage; //subtraindo o alvo pelo dano restante
+        target.health -= damage
 
-        if (target.health < 0) {
-            target.health = 0; // Garante que a saúde não seja negativa
-        }
-
-        console.log(`${target.name} tem ${target.health} pontos de vida restantes.`);
+        console.log(`${targetCharacter.name} have ${targetCharacter.health} points of life left.`)
     }
     }
 
 const character1 = new Thief(100, 150, 200)
 const character2 = new Character('Rat', 200, 20, 100)
 
-console.log(character1.attack(character2))
+console.log(character1.attack(character2)) 
 
 
